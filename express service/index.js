c0b930d64b1342a775e0c1c7d835b533f85b199c
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2017-11-27 20:49:24 -0500

    cambios

--- express service/index.js
+++ express service/index.js
@@ -2,18 +2,33 @@
 const express = require ('express')
 const app = express()
 var mysql = require('mysql');
+var dc = require('dc');
+var d3 = require('d3');
 var connection = mysql.createConnection ({
 	host	: 'localhost',
 	user	: 'root',
-	password :	''
+	password :	'',
+	 database: 'datosJaveriana'
 });
 connection.connect;
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  var arreglo = [];
+connection.query('SELECT * from estudiantescarreras', function(err, rows, fields) {
   if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
-});
 
+  for (var i = 0; i <rows.length ; i++)
+  	{
+  		arreglo.push(rows[i]);
+  	}
+  	var fs = require('fs');
+  	var myJsonString = JSON.stringify(arreglo);
+  	fs.writeFile('data.json', myJsonString, function(err) {
+  	if (err) throw err;
+});
+});
 connection.end();
-app.get('/', (req, res) => res.send('Hello World!'))
+//delay(1000);
+console.log(arreglo);
+ d3.values = arreglo;
+ console.log(d3.values);
+//app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(3000, () => console.log('Example'))
